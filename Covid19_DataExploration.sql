e6461710a47891908432589f24c6803ff7847a6c
Nafis Saami Azad <nafissazad@gmail.com>
2023-07-11 16:57:31 +0600

    Fixed errors

--- Covid19_DataExploration.sql
+++ Covid19_DataExploration.sql
@@ -19,22 +19,22 @@ Where continent is not null
 order by 1,2
 
 
--- Total Cases vs Total Deaths
+-- Total Cases vs Total Deaths in Bangladesh
 -- Shows likelihood of dying if you contract covid in your country
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From CovidDeaths$
-Where location like '%states%'
+Where location like '%bangladesh%'
 and continent is not null 
 order by 1,2
 
 
--- Total Cases vs Population
+-- Total Cases vs Population in Bangladesh
 -- Shows what percentage of population infected with Covid
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From CovidDeaths$
---Where location like '%states%'
+Where location like '%bangladesh%'
 order by 1,2
 
 
@@ -42,7 +42,6 @@ order by 1,2
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From CovidDeaths$
---Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -51,7 +50,6 @@ order by PercentPopulationInfected desc
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From CovidDeaths$
---Where location like '%states%'
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
@@ -75,12 +73,13 @@ order by TotalDeathCount desc
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From CovidDeaths$
---Where location like '%states%'
 where continent is not null 
---Group By date
 order by 1,2
 
+-- Check CovidVaccinations table
 
+SELECT *
+FROM CovidVaccinations$
 
 -- Total Population vs Vaccinations
 -- Shows Percentage of Population that has recieved at least one Covid Vaccine
@@ -89,11 +88,12 @@ Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinatio
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
 From CovidDeaths$ dea
-Join Covid19..CovidVaccinations$ vac
+Join CovidVaccinations$ vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
 order by 2,3
+
 
 
 -- Using CTE to perform Calculation on Partition By in previous query
@@ -105,7 +105,7 @@ Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinatio
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
 From CovidDeaths$ dea
-Join PortfolioProject..CovidVaccinations vac
+Join CovidVaccinations$ vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
